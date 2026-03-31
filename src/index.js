30d10f81b3b1bc0595610702b4b111313e2551ba
Darkdevilangel14 <kaylahjjacobs@gmail.com>
2026-03-31 22:12:51 +0200

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-//alert("Hello World");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "La tombe dit a la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#poem-generator-form");
+formElement.addEventListener("submit", generatePoem);
