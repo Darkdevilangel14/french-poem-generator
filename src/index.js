75b0593cb3909e6dbed477fb7f2b0929755133c2
Darkdevilangel14 <kaylahjjacobs@gmail.com>
2026-03-31 23:52:41 +0200

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generatePoem(event) {
     "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Sign the powem with 'SheCodes AI'in bold.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating a French poem about ${instructionsInput.value}`;
+
   console.log("Generating poem");
   axios.get(apiUrl).then(displayPoem);
 }
